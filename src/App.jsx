6f2adc10128680fc250ce4a496e9a165ac365a7f
donyamardani <174435998+donyamardani@users.noreply.github.com>
2026-08-05 01:14:48 +0330

    create basic navbar
    create basic navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Hero from '@/Sections/Hero'
-import About from '@/Sections/About'
-import Experience from '@/Sections/Experience'
-import Progect from '@/Sections/Progect'
-import Testimonials from '@/Sections/Testimonials'
-import Contact from '@/Sections/Contact'
+import Hero from './Sections/Hero'
+import About from './Sections/About'
+import Experience from './Sections/Experience'
+import Progect from './Sections/Progect'
+import Testimonials from './Sections/Testimonials'
+import Contact from './Sections/Contact'
+import Navbar from './Layout/Navbar'
 
 export default function App() {
   return (
--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
 export default function Navbar() {
+    const navlinks = [
+        {href: '#home', label: 'Home'},
+        {href: '#about', label: 'About'},
+        {href: '#experience', label: 'Experience'},
+        {href: '#projects', label: 'Projects'},
+        {href: '#testimonials', label: 'Testimonials'},
+        {href: '#contact', label: 'Contact'},
+    ]
   return (
-    <div>Navbar</div>
+    <header className={"fixed top-0 left-0 right-0  bg-transparent py-5 shadow-md z-50"}>
+        <nav className={"container mx-auto px-6 flex items-center justify-between"}>
+            <a className={"text-xl font-bold tracking-tight hover:text-primary"} href="#">
+              PM<span className='text-primary'>.</span>
+            </a>
+            {/* desctop nav */}
+            <div className={"flex items-center gap-1"}>
+                <div className={"glass"}>
+                    {navlinks.map((link,index)=>{
+                        return <a href={link.href} key={index}>{link.label}</a>
+                    })}
+                </div>
+            </div>
+        </nav>
+    </header>
   )
 }
